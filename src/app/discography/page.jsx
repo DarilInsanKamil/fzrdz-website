7b8e17a4d425fe9004d21e3300d9e3fec9e6a36e
Darill <darilinsankamil2132@gmail.com>
2023-08-24 15:19:22 +0700

    fixing

--- src/app/discography/page.jsx
+++ src/app/discography/page.jsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
 import React from "react";
 import styles from "./disco.module.css";
 import Image from "next/image";
 import flood_into from "../../assets/image/flood_into.svg";
 import break_cover from "../../assets/image/break.png";
-import play from '../../assets/icon/play.svg';
+import play from "../../assets/icon/play.svg";
 import { Button, ButtonIcon } from "@/components";
+import Link from "next/link";
 
 export default function Disocgraphy() {
   return (
@@ -13,6 +14,7 @@ export default function Disocgraphy() {
       <div className={styles.cover}>
         <Image
           src={flood_into}
+          className={styles.image}
           width={"auto"}
           height={"auto"}
           alt="flood_into_cover"
@@ -21,12 +23,15 @@ export default function Disocgraphy() {
         <div className={styles.player}>
           <p className={styles.type}>Single</p>
           <h3 className={styles.title_cover}>Flood Into</h3>
-          <ButtonIcon icon={play} alt={"play-icon"} title={"Play"} />
+          <Link href="https://open.spotify.com/intl-id/album/5wBZdQtJp4ad73MspHd7DG">
+            <ButtonIcon icon={play} alt={"play-icon"} title={"Play"} />
+          </Link>
         </div>
       </div>
       <div className={styles.cover}>
         <Image
           src={break_cover}
+          className={styles.image}
           width={"auto"}
           height={"auto"}
           alt="break_cover"
@@ -35,12 +40,15 @@ export default function Disocgraphy() {
         <div className={styles.player}>
           <p className={styles.type}>EP</p>
           <h3 className={styles.title_cover}>BREAK!</h3>
-          <ButtonIcon icon={play} alt={"play-icon"} title={"Play"} />
+          <Link href="https://open.spotify.com/intl-id/album/3E6nSXNMIVy4wofjSstjka">
+            <ButtonIcon icon={play} alt={"play-icon"} title={"Play"} />
+          </Link>
         </div>
       </div>
       <div className={styles.cover}>
         <Image
           src={break_cover}
+          className={styles.image}
           width={"auto"}
           height={"auto"}
           alt="break_cover"
@@ -49,7 +57,9 @@ export default function Disocgraphy() {
         <div className={styles.player}>
           <p className={styles.type}>Single</p>
           <h3 className={styles.title_cover}>Come Apart</h3>
-          <ButtonIcon icon={play} alt={"play-icon"} title={"Play"} />
+          <Link href="https://open.spotify.com/intl-id/track/3yJqlmzdenTmQeb3bdRJfO">
+            <ButtonIcon icon={play} alt={"play-icon"} title={"Play"} />
+          </Link>
         </div>
       </div>
     </div>
